1e68bc5a195afc7143c0f30cd4a648939f2fbfa4
ThirdChyr <162368012+ThirdChyr@users.noreply.github.com>
2024-03-23 23:03:45 +0700

    update Main back

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import{Card} from 'react-native-paper';
+import Setto from './frontend/setting.js';
+import Login from './frontend/Login.js';
+import Month from './frontend/Month.js';
+import Register from './frontend/register.js';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Card><Text>Open udwdadwadp App.js to start working on your app!</Text></Card>
       <StatusBar style="auto" />
+    {/* <Login/> */}
+    {/* <Setto/> */}
+    {/* <Register/> */}
+    <Month/>
     </View>
   );
 }
-
