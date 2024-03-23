b63e993ad722f1875e9e193a14ea513f99fbb271
Supaiwt <phone2546.pp@gmail.com>
2024-03-23 22:46:18 +0700

    LoginAndCreatACC

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import{Card} from 'react-native-paper';
+import React, { useState } from 'react';
+import Login from './frontend/Login.js';
+import Register from './frontend/Register.js';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Card><Text>Open udwdadwadp App.js to start working on your app!</Text></Card>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(<Register/>)
+  
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
