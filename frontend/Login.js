3ec3a55a2817bd176d8f4eb792f3770c254cd090
Supaiwt <phone2546.pp@gmail.com>
2024-03-25 17:51:09 +0700

    UpImageBackground

--- frontend/Login.js
+++ frontend/Login.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TextInput, Button,Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View,TextInput, Button,ImageBackground, SafeAreaView } from 'react-native';
 import React,{useState,useEffect} from 'react';
 import { Card, Title } from 'react-native-paper';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import bg from './login-piccard.png'
 export default function App() {
-    const Profile ={ uri:'https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg',
-    width: 250,
-    height: 150,};
 
     const [username,setUsername]=useState("");
     const [password,setPassword]=useState("");
@@ -20,10 +18,11 @@ export default function App() {
         <SafeAreaView style={styles.container}>
             {/* <Text>Teest login</Text> */}
 
-              <View style={styles.cardContainer}>
+              {/* <View style={styles.cardContainer}> */}
 
-                  <Card style={styles.card1}>
+                  {/* <Card style={styles.card1}> */}
 
+            <ImageBackground source={bg} style={styles.container1}>
                     <View style={styles.cardContainer}>
                       <View style={styles.container}>
 
@@ -71,13 +70,10 @@ export default function App() {
                       </Card>
                       </View>
                     </View>
-                    <View style={styles.center}>
+            </ImageBackground>
+                  {/* </Card> */}
 
-                    <Text style={styles.text}>Login</Text>
-                    </View>
-                  </Card>
-
-              </View>
+              {/* </View> */}
            
         </SafeAreaView>
     );
@@ -87,9 +83,18 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: 'center',
-      alignItems: 'center',
+      alignItems: 'flex-end',
       backgroundColor: 'no',
-      padding: 8,
+      padding: 60,
+      // borderWidth:1
+    },
+    container1: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+      backgroundColor: 'pink',
+      padding: 60,
+      // borderWidth:1
     },
     cardContainer: {
       flexDirection: 'row',
@@ -115,8 +120,8 @@ const styles = StyleSheet.create({
       width:900,
       height:600,
       padding: 10,
-      borderWidth: 5,
-      borderRadius: 10,
+      borderWidth: 3,
+      borderRadius: 5,
       backgroundColor:"white"
     },
     cardContainer: {
@@ -135,5 +140,5 @@ const styles = StyleSheet.create({
     icon: { 
       marginLeft: 200, 
       marginTop:-20
-  }, 
+    },
   });
