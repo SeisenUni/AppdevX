9520d8495897d4d8ef7857b0ecd3e870e08850ef
ThirdChyr <162368012+ThirdChyr@users.noreply.github.com>
2024-04-01 02:54:10 +0700

    problem found we cannot get month now send back to building compareing

--- frontend/Month.js
+++ frontend/Month.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
 import { Card, Title, Paragraph } from 'react-native-paper';
-import { oldd, newm, oldm, gotmonth, gotcommand ,sendcomand} from './keytime.js';
+import { oldd, newm, oldm, gotmonth, startd, gettd, stacked, dequeue } from './keytime.js';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
@@ -14,71 +14,92 @@ export default function App() {
         axios.get('http://192.168.227.165:5000/get_by_user')
             .then(response => {
                 const infoArray = response.data;
-                setAllData(infoArray); // เก็บข้อมูลลงใน state
+                setAllData(infoArray);
             })
             .catch(error => {
                 console.error('Error fetching info:', error);
             });
     }, []);
 
-    useEffect(() => {
-        allData.forEach(info => {
-            const canCreate = checkcancreate(info.date_start, info.date_end);
-            console.log("create is = ", canCreate);
-            gotcommand(canCreate);
-        });
-    }, [allData]);
 
+
+
+    // const list = Array.from({ length: 13 }, () => Array(40).fill());
 
     const data = [];
     for (let i = 1; i < 40; i++) {
         data.push({ key: i, title: `${i}` });
     }
     var countday = 0;
-    const checkcancreate = (startData, endData) => {
-        data.some(item => {
-            const number = Number(item.title);
-            const StartData = Number(startData.substring(0, 2));
-            const EndData = Number(endData.substring(0, 2));
-            console.log("number = ", number);
-            console.log("start = ", StartData);
-            console.log("end = ", EndData);
-            if (number >= StartData && number <= EndData) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-    }
+    // let send = 
+    // const send =()=>{
+    // return 
+    // }
+    // const get =(start)=>{
+    //     return
+    // }
+    // const checkcancreate = (startData, endData) => {
+    //     data.some(item => {
+    //         const number = Number(item.title);
+    //         const StartData = Number(startData.substring(0, 2));
+    //         const EndData = Number(endData.substring(0, 2));
+    //         const start = Number(startData.substring(3, 5));
+    //         const end = Number(startData.substring(3, 5));
+    //         const month = gotmonth();
+    //         if (number >= StartData && number <= EndData && (start >= Number(gotmonth()) && end <= Number(gotmonth()))) 
+    //         {
+    //            const number = Number(item.title);
+    //            const EndData = Number(endData.substring(0, 2));
+    //            startd(number);
+    //            stacked(EndData);
+    //            return 1;
 
-   
-    const createday = ({ item }) => {
-      
-        let newday = (item.title - countday);
-
-        if (countday >= oldd() && item.title <= newm() + 2) {
-            
-            let cp1 = newday + '-' + gotmonth() + '-' + '2024';
-                let month1 = gotmonth();
-                let day1 = newday;
-                //console.log(month1);
-                console.log(day1);
-                if (newday >= 1 && newday <= 10 && gotmonth() == 2) {
-                    return (
-                        <Card style={styles.selectday}>
-                            <Title style={styles.textst}>{newday}</Title>
-                            <Paragraph>Algorithm</Paragraph>
-                            <Paragraph>18:30 - 19:40</Paragraph>
-                        </Card>
-                    );
-
-                } else {
-                    return (
-                        <Card style={styles.card}>
-                            <Title style={styles.textst}>{newday}</Title>
-                        </Card>
-                    );
+    //         } else {
+    //             return 0;
+    //         }
+    //     });
+    // }
+    // let valuebuild = 0 ;
+    useEffect(() => {
+        allData.forEach(info => {
+            data.forEach(item => {
+                console.log("check");
+                if (item.title <= Number(info.date_start.substring(0, 2))) {
+                    console.log("getnow");
                 }
+                else {
+
+                }
+            });
+
+        });
+    }, [allData]);
+
+
+    const createday = ({ item }) => {
+        // console.log("kuy ",dequeue());
+        // console.log("wher is td ",gettd());
+
+        //console.log(dequeue());
+        if (countday >= oldd() && item.title <= newm() + 2) {
+            let newday = (item.title - countday);
+            //console.log("neday ",newday);
+            if (newday) {
+                return (
+                    <Card style={styles.selectday}>
+                        <Title style={styles.textst}>{newday}</Title>
+                        <Paragraph>Algorithm</Paragraph>
+                        <Paragraph>18:30 - 19:40</Paragraph>
+                    </Card>
+                );
+
+            } else {
+                return (
+                    <Card style={styles.card}>
+                        <Title style={styles.textst}>{newday}</Title>
+                    </Card>
+                );
+            }
         }
         else {
             countday++;
@@ -124,7 +145,8 @@ export default function App() {
                     renderItem={(item) => createday(item)}
                     numColumns={7}
                     contentContainerStyle={styles.flatListContent}
-                /></Card>
+                />
+            </Card>
             <StatusBar style="auto" />
 
         </SafeAreaView>
