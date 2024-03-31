6b50824e36ff4edb38416884892c400594320020
Supaiwt <phone2546.pp@gmail.com>
2024-03-31 20:09:33 +0700

    -bar

--- frontend/week.js
+++ frontend/week.js
@@ -376,12 +376,12 @@ export default function App() {
                                         <TextInput style={styles.input1}
                                             onChangeText={setStart}
                                             value={start}
-                                            placeholder="Start:  dd/mm/yyyy"
+                                            placeholder="Start:  dd-mm-yyyy"
                                             placeholderTextColor={'black'}/>
                                         <TextInput style={styles.input2}
                                         onChangeText={setEnd}
                                         value={end}
-                                        placeholder="End:  dd/mm/yyyy"
+                                        placeholder="End:  dd-mm-yyyy"
                                         placeholderTextColor={'black'}/>
                                         
                                         </Card>
