c1b7d77063c913c8df67fe2b1784e2787b77b858
Supaiwt <phone2546.pp@gmail.com>
2024-04-01 04:46:30 +0700

    day_int

--- backend/back.py
+++ backend/back.py
@@ -198,14 +198,16 @@ def add_task():
     str1=data["start"]
     str2 =str1[0:2]
     int(str2)
-    str3 =str1[3:2]
-    int(str3)
     
-    str6=data["end"]
-    str4 =str6[0:2]
+    str4 =str1[3:5]
     int(str4)
-    str5 =str6[3:2]
+    
+    str3=data["end"]
+    str5 =str3[0:2]
     int(str5)
+    
+    str6 =str3[3:5]
+    int(str6)
 
     new_task = {
         "_id": ttt,
@@ -216,10 +218,10 @@ def add_task():
         "date_start": data["start"],
         "date_end": data["end"],
         "time" : time ,
-        "day_start": str2,
-        "month_start":str3,
-         "day_end": str4,
-        "month_end":str5,
+        "day_start": int(str2),
+        "month_start": int(str4),
+        "day_end": int(str5),
+        "month_end":int(str6)
     }
     info_plan.append(new_task)
     try:
