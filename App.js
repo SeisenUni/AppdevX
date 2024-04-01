551b4bf793a8803092b77a153d50f9983e975880
ThirdChyr <162368012+ThirdChyr@users.noreply.github.com>
2024-04-01 23:43:02 +0700

    Delete some file not use

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import{Card} from 'react-native-paper';
-import Setto from './frontend/setting.js';
 import Login from './frontend/Login.js';
-import Month from './frontend/Month.js';
 import List from './frontend/list.js';
 import Register from './frontend/register.js';
-import Mainmonth from './frontend/MainMonth.js';
-import Bar from './frontend/menubar.js';
+import Month from './frontend/menubar.js';
 import Reset from './frontend/resetpass.js';
 import Remail from './frontend/resetmail.js';
 import Yearbar from './frontend/yearbar.js';
 import Week from './frontend/week.js';
-import Adding from './frontend/adding.js';
 import Forget from './frontend/forget.js'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -26,30 +20,15 @@ export default function App() {
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
         <Stack.Screen name="Regis" component={Register} options={{ headerShown: false }}/>
         <Stack.Screen name="Year"  component={Yearbar} options={{ headerShown: false }}/>
-        <Stack.Screen name="Bar"  component={Bar} options={{ headerShown: false }}/>
+        <Stack.Screen name="Month"  component={Month} options={{ headerShown: false }}/>
         <Stack.Screen name="Week"  component={Week} options={{ headerShown: false }}/>
-        <Stack.Screen name="Setto"  component={Setto} /*options={{ headerShown: false }}*//>
-        <Stack.Screen name="Adding"  component={Adding} /*options={{ headerShown: false }}*//>
-        <Stack.Screen name="ResetPass" component={Reset} options={{ headerShown: true }}/>
-        <Stack.Screen name="Changemail" component={Remail} options={{ headerShown: true }}/>
-        <Stack.Screen name="Forget" component={Forget} options={{ headerShown: true }}/>
+        <Stack.Screen name="ResetPass" component={Reset} options={{ headerShown: false }}/>
+        <Stack.Screen name="Changemail" component={Remail} options={{ headerShown: false }}/>
+        <Stack.Screen name="Forget" component={Forget} options={{ headerShown: false }}/>
         <Stack.Screen name="List"  component={List} options={{ headerShown: false }}/>
 
       </Stack.Navigator>
     </NavigationContainer>
-    // <View style={styles.container}>
-    //{/* <Bar/> */}
-   // {/* <Yearbar/> */}
-   //{/* <Login/> */}
-    //{/* <Setto/> */}
-//{/* <Register/> */}
-    //{/* <Month/> */}
-   //{/* <Mainmonth/> */}
-  // {/* <Reset/> */}
-  //  {/* <Mainmonth/> */}
-  //  {/* <Reset/> */}
-  //  {/* <Remail/> */}
-    // </View>
   );
 }
 const styles = StyleSheet.create({
