b8187723410101f6a7868218995d27a0612299e7
ThirdChyr <162368012+ThirdChyr@users.noreply.github.com>
2024-04-01 15:57:38 +0700

    fix month.js bec it about time error vdo ;-;

--- frontend/Month.js
+++ frontend/Month.js
@@ -1,189 +1,31 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
 import { Card, Title, Paragraph } from 'react-native-paper';
-import { oldd, newm, oldm, gotmonth, startd, gettd, stacked, dequeue } from './keytime.js';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { oldd, newm, oldm, gotmonth} from './keytime.js';
+
+
 
 export default function App() {
-    
 
-
-//     useEffect(() => {
-//         axios.get('http://192.168.227.165:5000/get_by_user')
-//             .then(response => {
-//                 const infoArray = response.data;
-//                 setAllData(infoArray);
-//             })
-//             .catch(error => {
-//                 console.error('Error fetching info:', error);
-//             });
-//     }, []); 
-
-const [key, setKey] = useState(999);
-const [newpost, setStart] = useState(999);
-const [rmonth, getmonth] = useState(999);
-
-
-// for(let i = 1;i<40;i++)
-// {
-//     useEffect(() => {
-//         axios.post('http://192.168.227.165:5000/test',{value : i})
-//             .then(response => {
-              
-//                 console.log("success i sad");
-//                 setKey(1); 
-//             })
-//             .catch(error => {
-//                 console.log("nokub i sad");
-    
-//                 //setKey(0); 
-//             });
-//     }, []);
-// }
-
-
-
-
-
-    // const list = Array.from({ length: 13 }, () => Array(40).fill());
-
-    
-    // let send = 
-    // const send =()=>{
-    // return 
-    // }
-    // const get =(start)=>{
-    //     return
-    // }
-    // const checkcancreate = (startData, endData) => {
-    //     data.some(item => {
-    //         const number = Number(item.title);
-    //         const StartData = Number(startData.substring(0, 2));
-    //         const EndData = Number(endData.substring(0, 2));
-    //         const start = Number(startData.substring(3, 5));
-    //         const end = Number(startData.substring(3, 5));
-    //         const month = gotmonth();
-    //         if (number >= StartData && number <= EndData && (start >= Number(gotmonth()) && end <= Number(gotmonth()))) 
-    //         {
-    //            const number = Number(item.title);
-    //            const EndData = Number(endData.substring(0, 2));
-    //            startd(number);
-    //            stacked(EndData);
-    //            return 1;
-
-    //         } else {
-    //             return 0;
-    //         }
-    //     });
-    // }
-    // let valuebuild = 0 ;
-    // useEffect(() => {
-    //     allData.forEach(info => {
-    //         data.forEach(item => {
-    //             console.log("check");
-    //             if (item.title <= Number(info.date_start.substring(0, 2))) {
-    //                 console.log("getnow");
-    //             }
-    //             else {
-
-    //             }
-    //         });
-
-    //     });
-    // }, [allData]);
-    const route=(value)=>
-    {
-        switch(value)
-        {
-            case 1: return '01';
-            case 2: return '02';
-            case 3: return '03';
-            case 4: return '04';
-            case 5: return '05';
-            case 6: return '06';
-            case 7: return '07';
-            case 8: return '08';
-            case 9: return '09';
-            default :return value;
-
-        }
-    }
     const data = [];
     for (let i = 1; i < 40; i++) {
         data.push({ key: i, title: `${i}` });
     }
-    var countday = 0;
-    const createonselect=(value)=>
-    {
-        console.log("flow this = ",value);
-        return (
-            <Card style={styles.selectday}>
-                <Title style={styles.textst}>{value}</Title>
-                <Paragraph>Algorithm</Paragraph>
-                <Paragraph>18:30 - 19:40</Paragraph>
-            </Card>
-        );
-    }
-    const createnormal=(value)=>
-    {
-        return (
-            <Card style={styles.card}>
-                <Title style={styles.textst}>{value}</Title>
-            </Card>
-        );
-    }
+    let countday =0;
     const createday = ({ item }) => {
         let newday = item.title - countday;
-        let start = route(newday) + '-' + gotmonth();
-        console.log(start);
         let time = 1;
         
     
-        if (countday >= oldd() && item.title <= newm() + 2) {
-            // console.log(newday);
-        axios.post("http://192.168.227.165:5000/date_check",{day : route(newday),month :gotmonth()})
-        .then(response => {
-                  
-            //console.log("get la i sus",response.day);
-           // createonselect(newday);
-            setStart(newday);
-            console.log(time);
-             setKey(3);
-             getmonth(gotmonth());
-            // console.log(rmonth);
-             //time++;
-             //return;
-            // return;
-            // setKey(9999);
-        })
-        .catch(error => {
-           // console.log("nokub i sad");
-        //    createnormal(newday)
-        //time++;
-            //setKey(0); 
-        });
-        // if(newday < key)
-        // {
-        console.log(Number(gotmonth()));
-            if ( (newpost+key >= newday && newday >= newpost) && rmonth == Number(gotmonth()) )   
-            {
-                return (
-                    <Card style={styles.selectday}>
-                        <Title style={styles.textst}>{newday}</Title>
-                        <Paragraph>Have someting...</Paragraph>
-                    </Card>
-                );
-            } else {
-                return (
-                    <Card style={styles.card} key={item.key}>
-                        <Title style={styles.textst}>{newday}</Title>
-                    </Card>
-                );
-            }
-        // }
-            
+        if (countday >= oldd() && item.title <= newm() + 2) 
+        {
+            return (
+                <Card style={styles.card} key={item.key}>
+                    <Title style={styles.textst}>{newday}</Title>
+                </Card>
+            );
+
         } else {
             countday++;
             if (item.title < oldm()) {
